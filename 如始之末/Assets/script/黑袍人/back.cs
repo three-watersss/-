603c13c0c3347fca26edb18c5d327128d3132587
three-watersss <2372784315@qq.com>
2022-12-30 21:54:46 +0800

    黑袍人场景完成

---
+++ 如始之末/Assets/script/黑袍人/back.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class back : MonoBehaviour
+{
+    public GameObject dialog;
+    public void enterdialog()
+    {
+        dialog.SetActive(true);
+    }
+}
---
+++ 如始之末/Assets/script/黑袍人/buffchoose1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buffchoose1 : MonoBehaviour
+{
+    public void backToChooseScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void buff1()
+    {
+
+    }
+    public void buff2()
+    {
+
+    }
+    public void buff3()
+    {
+
+    }
+}
