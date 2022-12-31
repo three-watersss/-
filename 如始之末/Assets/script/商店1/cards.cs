73df956526ad69615f0b4f55194249abeca31d39
three-watersss <2372784315@qq.com>
2022-12-31 20:25:02 +0800

    完成商店1

---
+++ 如始之末/Assets/script/商店1/cards.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cards : MonoBehaviour
+{
+    public void backToChooseScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void getCard1()
+    {
+
+    }
+    public void getCard2()
+    {
+
+    }
+    public void getCard3()
+    {
+
+    }
+}
+
---
+++ 如始之末/Assets/script/商店1/store.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class store : MonoBehaviour
+{
+    public GameObject cards;
+    public void enterstore()
+    {
+        cards.SetActive(true);
+    }
+}
